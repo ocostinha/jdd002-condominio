c5ff04f1c3c632e3e88067d1121759168f26c33a
felipecosta <fcosta.paltrinieri@gmail.com>
2025-10-27 20:25:25 -0300

    Set HTTP 201 status for CondominoController POST response

--- src/main/java/com/alura/condominio/exercicio/CondominoController.java
+++ src/main/java/com/alura/condominio/exercicio/CondominoController.java
@@ -1,9 +1,11 @@
 package com.alura.condominio.exercicio;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CondominoController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String cadastrarCondomino(@Valid @RequestBody ContratoEntradaCadastroCondomino condomino){
         return "Cadastro efetuado com sucesso";
     }
