417d7b50428587e8573a3818986434b8c27a6b9e
felipecosta <fcosta.paltrinieri@gmail.com>
2025-10-27 21:38:32 -0300

    Add new endpoints for updating, retrieving by CPF, and listing all residents in CondominoController

--- src/main/java/com/alura/condominio/exercicio/CondominoController.java
+++ src/main/java/com/alura/condominio/exercicio/CondominoController.java
@@ -2,9 +2,13 @@ package com.alura.condominio.exercicio;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,25 @@ public class CondominoController {
     @ResponseStatus(HttpStatus.CREATED)
     public String cadastrarCondomino(@Valid @RequestBody ContratoEntradaCadastroCondomino condomino){
         return "Cadastro efetuado com sucesso";
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String atualizarCondomino(@PathVariable String id,
+                                     @Valid @RequestBody ContratoEntradaCadastroCondomino condomino){
+        return "Cadastro atualizado com sucesso";
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String buscarCondominoPorCPF(@RequestParam("cpf") String cpf){
+        return "Lista de condominos";
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String buscarTodosCondomino(){
+        return "Lista de todos os condominos";
     }
 
 }
